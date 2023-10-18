6a43a30ecc9ebdafe5ea7863d5206693ed4a177b
efcode <128084445+efremt31@users.noreply.github.com>
2023-10-18 18:13:36 -0400

    first

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = ({ handleToggleDarkMode }) => {
+  return (
+    <div className="header">
+      <h1>Memos</h1>
+      <button
+        onClick={() =>
+          handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
+        }
+        className="toggle-btn"
+      >
+        Toggle Mode
+      </button>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/NotesList.js
@@ -0,0 +1,20 @@
+import Notes from "./Note";
+import AddNote from "./AddNote";
+
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
+  return (
+    <div className="notes-list">
+      {notes.map((note) => (
+        <Notes
+          id={note.id}
+          text={note.text}
+          date={note.date}
+          handleDeleteNote={handleDeleteNote}
+        />
+      ))}
+      <AddNote handleAddNote={handleAddNote} />
+    </div>
+  );
+};
+
+export default NotesList;
